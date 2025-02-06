980756c5011952648a49bcd60aa22d0e0ae9ff99
FlorinM02 <florinmuica@yahoo.com>
2025-02-06 21:52:21 +0200

    update

---
+++ 3.update.sql
@@ -0,0 +1,18 @@
+select * from unitati_acvatice;
+update unitati_acvatice set nume = 'Lacul Prisacaa' where id=1;
+update unitati_acvatice set nume = 'Lacul lu Iovanu' where id=2;
+update unitati_acvatice set nume = 'Raul Cerna Sector 1a' where id=3;
+update unitati_acvatice set nume = 'Raul Cerna Sector 2a' where id=4;
+update unitati_acvatice set nume = 'Raul Cerna Sector 3a' where id=5;
+update unitati_acvatice set nume = 'Raul Cerna Sector 4a' where id=6;
+update unitati_acvatice set nume = 'Raul Cerna Sector 5a' where id=7;
+update unitati_acvatice set nume = 'Raul Cerna Sector 6a' where id=8;
+update unitati_acvatice set nume = 'Raul Cerna Sector 7a' where id=9;
+update unitati_acvatice set nume = 'Raul Cerna Sector 8a' where id=10;
+
+
+
+
+
+
+
