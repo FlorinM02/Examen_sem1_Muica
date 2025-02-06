f806dcb39a3983f60c1a9fbe41edc92bec7be6f6
FlorinM02 <florinmuica@yahoo.com>
2025-02-06 19:26:19 +0200

    create_gata_bun

--- 1.create.sql
+++ 1.create.sql
@@ -1,18 +1,3 @@
-create table if not exists parc
-(
-	fid serial primary key,
-	nume varchar(100) unique not null,
-	suprafata decimal null
-);
-
-create table if not exists unitati_acvatice
-(
-	id serial primary key,
-	nume varchar(50) unique not null,
-	suprafata decimal null,
-	fid_parc integer not null
-);
-
 create table if not exists tip_ua
 (
 	id serial primary key,
