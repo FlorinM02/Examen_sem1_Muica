7821738e3ce0273aeb07abaed99e2bb8fcf8e493
FlorinM02 <florinmuica@yahoo.com>
2025-02-06 23:10:21 +0200

    sq

--- 6.subquery.sql
+++ 6.subquery.sql
@@ -1,3 +1,10 @@
 select * from unitati_acvatice;
 select * from unitati_acvatice where not (suprafata between 1 and 10 or suprafata> 11) and (nume like '%Lacul Prisacaa%');
 select * from unitati_acvatice where (suprafata between 1 and 10 or suprafata> 11) and not (nume like '%Lacul Prisacaa%');
+select count (*) as nr_sectoare from unitati_acvatice;
+select * from specie;
+select count (*) as tipuri_pesti from specie;
+select sum (suprafata) as suprafata_totala from unitati_acvatice;
+select min(suprafata) from crescatorii;
+select max(suprafata) from crescatorii;
+select avg(suprafata) from crescatorii;
