58ce984a64c3efd2897616687f125bf6410b8e1f
FlorinM02 <florinmuica@yahoo.com>
2025-02-06 19:52:57 +0200

    create_final

--- 1.create.sql
+++ 1.create.sql
@@ -10,7 +10,7 @@ create table if not exists unitati_acvatice
 	id serial primary key,
 	nume varchar(50) unique not null,
 	suprafata decimal null,
-	fid_parc integer not null
+	fid_parc integer not null references parc(fid)
 );
 
 create table if not exists tip_ua
@@ -18,19 +18,19 @@ create table if not exists tip_ua
 	id serial primary key,
 	tip varchar(30) not null,
 	suprafata decimal null,
-	id_unitati_acvatice integer not null
+	id_unitati_acvatice integer not null references unitati_acvatice(id)
 );
 
 create table if not exists cuib
 (
 	id serial primary key,
-	id_unitati_acvatice integer not null
+	id_unitati_acvatice integer not null references unitati_acvatice(id)
 );
 
 create table if not exists locuri_acces
 (
 	id serial primary key,
-	id_unitati_acvatice integer not null
+	id_unitati_acvatice integer not null references unitati_acvatice(id)
 );
 
 create table if not exists specie
@@ -38,14 +38,14 @@ create table if not exists specie
 	id serial primary key,
 	nume varchar(30) not null,
 	tip_specie varchar(20) not null,
-	id_tip_ua integer not null
+	id_tip_ua integer not null references tip_ua(id)
 );
 
 create table if not exists tip_specie
 (
 	id serial primary key,
 	tip varchar(30) not null,
-	id_specie integer not null
+	id_specie integer not null references specie(id)
 );
 
 create table if not exists crescatorii
@@ -53,7 +53,7 @@ create table if not exists crescatorii
 	id serial primary key,
 	suprafata decimal null,
 	tip_productie varchar(20) not null,
-	id_specie integer not null
+	id_specie integer not null references specie(id)
 );
 
 create table if not exists date_crescatorii
@@ -61,5 +61,5 @@ create table if not exists date_crescatorii
 	id serial primary key,
 	nume varchar(50) not null,
 	suprafata decimal null,
-	id_crescatorii integer not null
+	id_crescatorii integer not null references crescatorii(id)
 );
---
+++ 2.insert.sql
@@ -0,0 +1,17 @@
+insert into parc (fid, nume, suprafata) values ('1', 'Parcul National Domogled Valea Cernei', '61200');
+insert into unitati_acvatice (id, nume, suprafata, fid_parc) values ('1', 'Lacul Prisaca', '78', '1');
+insert into unitati_acvatice (id, nume, suprafata, fid_parc) values ('2', 'Lacul Iovan', '290', '1');
+insert into unitati_acvatice (id, nume, suprafata, fid_parc) values ('3', 'Raul Cerna Sector 1', '2', '1');
+insert into unitati_acvatice (id, nume, suprafata, fid_parc) values ('4', 'Raul Cerna Sector 2', '3', '1');
+insert into unitati_acvatice (id, nume, suprafata, fid_parc) values ('5', 'Raul Cerna Sector 3', '6', '1');
+insert into unitati_acvatice (id, nume, suprafata, fid_parc) values ('6', 'Raul Cerna Sector 4', '2', '1');
+insert into unitati_acvatice (id, nume, suprafata, fid_parc) values ('7', 'Raul Cerna Sector 5', '8', '1');
+insert into unitati_acvatice (id, nume, suprafata, fid_parc) values ('8', 'Raul Cerna Sector 6', '5', '1');
+insert into unitati_acvatice (id, nume, suprafata, fid_parc) values ('9', 'Raul Cerna Sector 7', '3', '1');
+insert into unitati_acvatice (id, nume, suprafata, fid_parc) values ('10', 'Raul Cerna Sector 8', '7', '1');
+insert into tip_ua (id,tip, id_unitati_acvatice) values ('1', 'Lac','1');
+insert into tip_ua (id,tip, id_unitati_acvatice) values ('2', 'Lac','2');
+insert into tip_ua (id,tip, id_unitati_acvatice) values ('3', 'Rau','3');
+insert into tip_ua (id,tip, id_unitati_acvatice) values ('4', 'Rau','4');
+insert into tip_ua (id,tip, id_unitati_acvatice) values ('5', 'Rau','5');
+insert into tip_ua (id,tip, id_unitati_acvatice) values ('1', 'Lac','1');
