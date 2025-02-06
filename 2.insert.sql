5ecab0f7fc16f662a8fb7112105878c5c82cfe1e
FlorinM02 <florinmuica@yahoo.com>
2025-02-06 21:30:01 +0200

    Update 2.insert.sql

--- 2.insert.sql
+++ 2.insert.sql
@@ -19,6 +19,38 @@ insert into tip_ua (id,tip, id_unitati_acvatice) values ('7', 'Rau','7');
 insert into tip_ua (id,tip, id_unitati_acvatice) values ('8', 'Rau','8');
 insert into tip_ua (id,tip, id_unitati_acvatice) values ('9', 'Rau','9');
 insert into tip_ua (id,tip, id_unitati_acvatice) values ('10', 'Rau','10');
+insert into cuib (id, id_unitati_acvatice) values ('1','1');
+insert into cuib (id, id_unitati_acvatice) values ('2','2');
+insert into cuib (id, id_unitati_acvatice) values ('3','3');
+insert into cuib (id, id_unitati_acvatice) values ('4','4');
+insert into cuib (id, id_unitati_acvatice) values ('5','5');
+insert into cuib (id, id_unitati_acvatice) values ('6','6');
+insert into cuib (id, id_unitati_acvatice) values ('7','7');
+insert into cuib (id, id_unitati_acvatice) values ('8','8');
+insert into cuib (id, id_unitati_acvatice) values ('9','9');
+insert into cuib (id, id_unitati_acvatice) values ('10','10');
+insert into locuri_acces (id,id_unitati_acvatice ) values ('1','1');
+insert into locuri_acces (id,id_unitati_acvatice ) values ('2','2');
+insert into locuri_acces (id,id_unitati_acvatice ) values ('3','3');
+insert into locuri_acces (id,id_unitati_acvatice ) values ('4','4');
+insert into locuri_acces (id,id_unitati_acvatice ) values ('5','5');
+insert into locuri_acces (id,id_unitati_acvatice ) values ('6','6');
+insert into locuri_acces (id,id_unitati_acvatice ) values ('7','7');
+insert into locuri_acces (id,id_unitati_acvatice ) values ('8','8');
+insert into locuri_acces (id,id_unitati_acvatice ) values ('9','9');
+insert into locuri_acces (id,id_unitati_acvatice ) values ('10','10');
+insert into specie (id, nume, tip_specie, id_tip_ua) values ('1', 'pastrav', 'rapitor', '3');
+insert into specie (id, nume, tip_specie, id_tip_ua) values ('2', 'clean', 'rapitor', '3');
+insert into specie (id, nume, tip_specie, id_tip_ua) values ('3', 'lipan', 'rapitor', '3');
+insert into specie (id, nume, tip_specie, id_tip_ua) values ('4', 'lostrita', 'rapitor', '7');
+insert into specie (id, nume, tip_specie, id_tip_ua) values ('5', 'salau', 'rapitor', '1');
+insert into specie (id, nume, tip_specie, id_tip_ua) values ('6', 'salau', 'rapitor', '2');
+insert into specie (id, nume, tip_specie, id_tip_ua) values ('7', 'crap', 'rapitor', '1');
+insert into specie (id, nume, tip_specie, id_tip_ua) values ('8', 'crap', 'rapitor', '2');
+insert into specie (id, nume, tip_specie, id_tip_ua) values ('9', 'caras', 'rapitor', '1');
+insert into specie (id, nume, tip_specie, id_tip_ua) values ('10', 'caras', 'rapitor', '2');
+
+
 
 
 
